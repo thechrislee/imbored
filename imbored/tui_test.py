51d6be8354361feed3e1d329de24ad47f59ddf89
Chris Lee <chris@noreply.github.com>
2022-10-31 11:07:18 -0400

    Modified code so that the tui would not crash when 'Event' was clicked on. https://github.com/thechrislee/imbored/issues/6

--- imbored/tui_test.py
+++ imbored/tui_test.py
@@ -32,19 +32,19 @@ class MyApp(App):
     async def on_mount(self, event: events.Mount) -> None:
 
         self.description = Markdown(
-            f"## Welcome to `imbored`. Click an event on the left to view the details of it. "
+            f"## Welcome to `imbored`. Click an event on the left to view the details of it."
         )
         self.body = ScrollView(self.description)
 
         tree = TreeControl("Events", {})
-        """
-        add custom events
-        """
+
         scraper = Scrape()
         event_data = scraper.get_events()
+        events = {}
         for event in event_data:
             e = Event.from_json(event)
             name = e.name
+            start_date = (str(e.startDate).split()[0]).strip()
             description = str(e)
             await tree.add(
                 tree.root.id, name, {"event": name, "description": description}
@@ -53,15 +53,22 @@ class MyApp(App):
 
         await self.view.dock(Header(), edge="top")
         await self.view.dock(Footer(), edge="bottom")
-        await self.view.dock(ScrollView(tree), size=30, edge="left")
+        await self.view.dock(ScrollView(tree), size=50, edge="left")
         await self.view.dock(self.body)
 
     async def handle_tree_click(self, message: TreeClick[dict]) -> None:
         ID = message.node.id
         label = message.node.label
         event = message.node.data.get("event")
-        self.description = Markdown(message.node.data.get("description"))
-        await self.body.update(self.description)
+        # self.description = Markdown(message.node.data.get("description"))
+        if ID == 0:
+            self.description = Markdown(
+                f"## Welcome to `imbored`. Click an event on the left to view the details of it."
+            )
+            await self.body.update(self.description)
+        else:
+            self.description = Markdown(message.node.data.get("description"))
+            await self.body.update(self.description)
 
 
 MyApp.run(title="imbored", log="textual.log")
